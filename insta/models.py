76d78fc6cf7ea366e5f642b4c3fc1bc669348775
luqqy <you@example.com>
2020-10-19 12:00:13 +0300

    made migrations for changes made

--- insta/models.py
+++ insta/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 class post(models.Model):
     author =models.Foreignkey('auth.user',on_delete=models.CASCADE)
     image = models.imageField(blank=True, null=True)
+    caption = models.TextField()
+    created_date = models.outerTimeField(default=timezone.now)
