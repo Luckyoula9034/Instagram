b5c240a39254aead979ea5cd9382224547020867
luqqy <you@example.com>
2020-10-19 12:46:33 +0300

    added def __str__(self): to return a string representaion of the object

--- insta/models.py
+++ insta/models.py
@@ -8,3 +8,7 @@ class post(models.Model):
     image = models.ImageField(blank=True, null=True)
     caption = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+       
+        return self.caption
