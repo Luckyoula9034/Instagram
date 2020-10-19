cb79aae0d27e6eb0abf14b1d91490a9938dde1a1
luqqy <you@example.com>
2020-10-19 12:57:06 +0300

    created templates folder

--- insta/views.py
+++ insta/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
 from .models import post
+from django.views.generic import (
+    ListView
+)
 
 # Create your views here.
+class Postlistviews(ListView):
+    template_name = "insta/post_list.html"
+    queryset = Post.objects.all()
+    context_object_name ='posts'
