38f1a88a3f8bb804c10c414cc39b152e5796e4ec
luqqy <you@example.com>
2020-10-19 12:36:57 +0300

    register class post from models in the admin section

--- insta/models.py
+++ insta/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 
 class post(models.Model):
-    author =models.Foreignkey('auth.user',on_delete=models.CASCADE)
-    image = models.imageField(blank=True, null=True)
+    author = models.ForeignKey('auth.user',on_delete=models.CASCADE)
+    image = models.ImageField(blank=True, null=True)
     caption = models.TextField()
-    created_date = models.outerTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
