39adeff5ea97bf259d252f3316a0f674493d6284
luqqy <you@example.com>
2020-10-21 11:42:22 +0300

    deploying to heroku

--- instagram/settings.py
+++ instagram/settings.py
@@ -120,8 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfile') 
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
 
 MEDIA_URL = '/media/'
 
